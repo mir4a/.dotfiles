7f6b42ce0407c5e8f640a0be0b310632a51055f3
Miroslav Martynov <miroslav.martynov@4finance.com>
2021-01-14 18:37:01 +0100

    style: 💄 cleanup

--- setup-symlink.sh
+++ setup-symlink.sh
@@ -3,8 +3,8 @@
 # Create directories if do not exist yet	# Create directories if do not exist yet
 [ -d ~/Library/Application\ Support/Code/User/ ] || mkdir ~/Library/Application\ Support/Code/User
 
-# Visual Studio Code	
-ln -s ~/.dotfiles/VSCode/* ~/Library/Application\ Support/Code/User	
+# Visual Studio Code
+ln -s ~/.dotfiles/VSCode/* ~/Library/Application\ Support/Code/User
 
 # Dotfiles
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
